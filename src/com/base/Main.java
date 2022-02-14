bf32a62512a93d272185336db9680da7596a4ac0
Szynaman123 <45926130+Szynaman123@users.noreply.github.com>
2022-02-14 16:28:12 +0100

    com

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -1,24 +1,56 @@
 package com.base;
+import org.apache.http.HttpException;
+import org.apache.http.protocol.HTTP;
 import org.apache.jena.query.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import org.apache.jena.query.QueryFactory.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ModelReader;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ParameterizedSparqlString qs = new ParameterizedSparqlString(""
-                + "PREFIX dbres: <http://dbpedia.org/resource/>\n"
-                + "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-                + "select ?o where { dbres:Drug rdf:type ?o}LIMIT 10");
+                + "PREFIX dbr: <http://dbpedia.org/resource/>\n"
+                + "PREFIX dbo:<http://dbpedia.org/ontology/>\n"
+                + "SELECT DISTINCT *"
+                + "WHERE {"
+                + "?subject a dbo:Person."
+                + "?subject ?predicate ?object." +
+                "FILTER (?subject IN (dbr:John_Lennon, dbr:Yoko_Ono, dbr:Jimmy_Carter,\n" +
+                "           dbr:Bill_Clinton) && lang(?object) = \"en\")" +
+                "} LIMIT 20");
+        Scanner odczyt = new Scanner(new File("C:\\Users\\szyna\\IdeaProjects\\RDF_and_SPARQL_test\\src\\com\\base\\endpoints"));
+        ArrayList<String>Endpoints=new ArrayList<String>();
+        while (odczyt.hasNextLine()) {
+            String text = odczyt.nextLine();
+            Endpoints.add(text);
+        };
+        ArrayList<Integer>indexes=new ArrayList<Integer>();
+        System.out.println(Endpoints.get(1).toString());
+        for(int i=0;i<Endpoints.size();i++){
+            try{
+                QueryExecution exec = QueryExecutionFactory.sparqlService(Endpoints.get(i).toString(), qs.asQuery());
 
-        QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", qs.asQuery());
-        ResultSet results = exec.execSelect();
-        while (results.hasNext()) {
+                ResultSet results = exec.execSelect();
+                while (results.hasNext()) {
 
-            System.out.println(results.next().get("o").toString());
-        }
-        ResultSetFormatter.out(results);
+                    System.out.println(results.next().get("subject").toString());
+                }
+                ResultSetFormatter.out(results);
+            }catch (Exception e){
+                indexes.add(i+1);
+            }
 
-    }
-}
+
+   }
+    System.out.println(indexes);
+    }}
+
