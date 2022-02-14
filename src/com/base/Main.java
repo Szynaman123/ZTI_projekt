280de22a0e9000687e8c5207228a20d23b8c7457
Szynaman123 <45926130+Szynaman123@users.noreply.github.com>
2022-02-14 20:47:19 +0100

    Querry wikiData & dbpedia

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -1,6 +1,7 @@
 package com.base;
 import org.apache.http.HttpException;
 import org.apache.http.protocol.HTTP;
+import org.apache.jena.base.Sys;
 import org.apache.jena.query.*;
 
 import java.io.File;
@@ -17,40 +18,81 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        ParameterizedSparqlString qs = new ParameterizedSparqlString(""
-                + "PREFIX dbr: <http://dbpedia.org/resource/>\n"
-                + "PREFIX dbo:<http://dbpedia.org/ontology/>\n"
-                + "SELECT DISTINCT *"
-                + "WHERE {"
-                + "?subject a dbo:Person."
-                + "?subject ?predicate ?object." +
-                "FILTER (?subject IN (dbr:John_Lennon, dbr:Yoko_Ono, dbr:Jimmy_Carter,\n" +
-                "           dbr:Bill_Clinton) && lang(?object) = \"en\")" +
-                "} LIMIT 20");
+        try{
+        ParameterizedSparqlString qs = new ParameterizedSparqlString("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+                "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+                "PREFIX p: <http://www.wikidata.org/prop/>\n" +
+                "PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
+                "PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+                "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "\n" +
+                "SELECT DISTINCT\n" +
+                "        (?person as ?wikidataID)\n" +
+                "        ?personLabel\n" +
+                "        ?dbpediaID\n" +
+                "WHERE{\n" +
+                "SERVICE <https://query.wikidata.org/sparql>\n" +
+                "          {\n" +
+                "SELECT DISTINCT ?person ?personLabel\n" +
+                "WHERE{\n" +
+                "\n" +
+                "  ?person wdt:P735 wd:Q18145837;\n" +
+                "   wdt:P734 wd:Q1621139; \n" +
+                "   wdt:P19 wd:Q131128.\n" +
+                "  \n" +
+                "\n" +
+                "  SERVICE wikibase:label \n" +
+                "          {\n" +
+                "            bd:serviceParam wikibase:language \"en\"\n" +
+                "          }\n" +
+                "  \n" +
+                "}}SERVICE <https://dbpedia.org/sparql>\n" +
+                "          {\n" +
+                "              SELECT ?dbpediaID ?label ?description\n" +
+                "                   WHERE { ?dbpediaID owl:sameAs ?person  ;\n" +
+                "                                      rdfs:label ?label ;\n" +
+                "                                      rdfs:comment ?description .\n" +
+                "                           FILTER (LANG(?label) = \"en\")\n" +
+                "                           FILTER (LANG(?description) = \"en\")\n" +
+                "                         }\n" +
+                "      }}"
+);
         Scanner odczyt = new Scanner(new File("C:\\Users\\szyna\\IdeaProjects\\RDF_and_SPARQL_test\\src\\com\\base\\endpoints"));
-        ArrayList<String>Endpoints=new ArrayList<String>();
+        ArrayList<String> Endpoints = new ArrayList<String>();
         while (odczyt.hasNextLine()) {
             String text = odczyt.nextLine();
             Endpoints.add(text);
-        };
-        ArrayList<Integer>indexes=new ArrayList<Integer>();
-        System.out.println(Endpoints.get(1).toString());
-        for(int i=0;i<Endpoints.size();i++){
-            try{
-                QueryExecution exec = QueryExecutionFactory.sparqlService(Endpoints.get(i).toString(), qs.asQuery());
+        }
+        ;
+        ArrayList<Integer> indexes = new ArrayList<Integer>();
+//        System.out.println(Endpoints.get(1).toString());
+//        for(int i=0;i<Endpoints.size();i++){
+//            try{
 
-                ResultSet results = exec.execSelect();
-                while (results.hasNext()) {
 
-                    System.out.println(results.next().get("subject").toString());
-                }
-                ResultSetFormatter.out(results);
-            }catch (Exception e){
-                indexes.add(i+1);
+        QueryExecution exec = QueryExecutionFactory.sparqlService("https://linkeddata.uriburner.com/sparql", qs.asQuery());
+
+        ResultSet results = exec.execSelect();
+//        while (results.hasNext()) {
+
+//            System.out.println(results.next().get("dbpediaID"));
+//        }
+            System.out.println(ResultSetFormatter.asText(results));
+//            }catch (Exception e){
+//                indexes.add(i+1);
             }
+    catch(Exception e){
+        System.out.println(e.toString());
+    }
 
 
-   }
-    System.out.println(indexes);
-    }}
+   }}
+//    System.out.println(indexes);
+
 
