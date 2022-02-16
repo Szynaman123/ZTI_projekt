8f72af0e25097248df5abd703db6b4c80e432312
Szynaman123 <45926130+Szynaman123@users.noreply.github.com>
2022-02-16 15:14:27 +0100

    Querry wikiData & dbpedia & Yago

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -6,6 +6,9 @@ import org.apache.jena.query.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import org.apache.jena.query.QueryFactory.*;
 import org.apache.jena.rdf.model.Model;
@@ -16,10 +19,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
+    public static ResultSet makeQuery(String queryString){
+        ParameterizedSparqlString qs = new ParameterizedSparqlString(queryString);
+        QueryExecution exec = QueryExecutionFactory.sparqlService("https://linkeddata.uriburner.com/sparql", qs.asQuery());
+        ResultSet results = exec.execSelect();
+        return results;
+    }
+    public static String txtFileToString(String filePath){
+        String text = "";
+        try {
+            text = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
     public static void main(String[] args) throws FileNotFoundException {
         try{
-        ParameterizedSparqlString qs = new ParameterizedSparqlString("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+        ResultSet results = makeQuery("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
                 "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
                 "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
                 "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
@@ -31,11 +48,14 @@ public class Main {
                 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
                 "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
                 "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "PREFIX sch: <http://schema.org/>\n" +
+                "PREFIX yago: <http://yago-knowledge.org/resource/> " +
                 "\n" +
                 "SELECT DISTINCT\n" +
-                "        (?person as ?wikidataID)\n" +
                 "        ?personLabel\n" +
+                "        (?person as ?wikidataID)\n" +
                 "        ?dbpediaID\n" +
+                "        ?yagoID\n" +
                 "WHERE{\n" +
                 "SERVICE <https://query.wikidata.org/sparql>\n" +
                 "          {\n" +
@@ -61,37 +81,181 @@ public class Main {
                 "                           FILTER (LANG(?label) = \"en\")\n" +
                 "                           FILTER (LANG(?description) = \"en\")\n" +
                 "                         }\n" +
-                "      }}"
-);
-        Scanner odczyt = new Scanner(new File("C:\\Users\\szyna\\IdeaProjects\\RDF_and_SPARQL_test\\src\\com\\base\\endpoints"));
-        ArrayList<String> Endpoints = new ArrayList<String>();
-        while (odczyt.hasNextLine()) {
-            String text = odczyt.nextLine();
-            Endpoints.add(text);
+                "      }SERVICE <https://yago-knowledge.org/sparql/query>\n" +
+                "{ \n" +
+                "SELECT ?yagoID\n" +
+                "WHERE { ?yagoID sch:givenName yago:Adolf;\n" +
+                "sch:birthPlace yago:Braunau_am_Inn;\n" +
+                "sch:spouse yago:Eva_Braun.\n" +
+                "} }}"
+);  System.out.println(ResultSetFormatter.asText(results));
         }
-        ;
-        ArrayList<Integer> indexes = new ArrayList<Integer>();
-//        System.out.println(Endpoints.get(1).toString());
-//        for(int i=0;i<Endpoints.size();i++){
-//            try{
-
-
-        QueryExecution exec = QueryExecutionFactory.sparqlService("https://linkeddata.uriburner.com/sparql", qs.asQuery());
-
-        ResultSet results = exec.execSelect();
-//        while (results.hasNext()) {
-
-//            System.out.println(results.next().get("dbpediaID"));
-//        }
-            System.out.println(ResultSetFormatter.asText(results));
-//            }catch (Exception e){
-//                indexes.add(i+1);
-            }
     catch(Exception e){
         System.out.println(e.toString());
     }
 
+        ResultSet results1 = makeQuery("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+                "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+                "PREFIX p: <http://www.wikidata.org/prop/>\n" +
+                "PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
+                "PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+                "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "PREFIX sch: <http://schema.org/>\n" +
+                "PREFIX yago: <http://yago-knowledge.org/resource/> \n" +
+                "SELECT DISTINCT\n" +
+                "        ?wdLabel\n" +
+                "WHERE{\n"+
+                "SERVICE <https://query.wikidata.org/sparql>\n" +
+                "{\n" +
+                "SELECT DISTINCT ?wdLabel  {\n" +
+                "  VALUES (?person) {(wd:Q352)}\n" +
+                "  ?person ?p ?statement .\n" +
+                "  ?wd wikibase:claim ?p.\n" +
+                "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" }\n" +
+                "} ORDER BY ?wd ?statement "+
+                "}}"
+                 );
+        int counter=0;
+        while (results1.hasNext()) {
+            results1.next();
+            counter++;
 
+     }
+    System.out.println(counter);
+
+        ResultSet results2 = makeQuery("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+                "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+                "PREFIX p: <http://www.wikidata.org/prop/>\n" +
+                "PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
+                "PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+                "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "PREFIX sch: <http://schema.org/>\n" +
+                "PREFIX yago: <http://yago-knowledge.org/resource/> \n" +
+                "SELECT DISTINCT\n" +
+                "        ?property\n" +
+                "WHERE{\n"+
+                "SERVICE <https://dbpedia.org/sparql>\n" +
+                "{\n" +
+                "SELECT DISTINCT ?property  {\n" +
+                "{ <http://dbpedia.org/resource/Adolf_Hitler> ?property ?value. }\n" +
+                "UNION\n" +
+                "{ ?value ?property <http://dbpedia.org/resource/Adolf_Hitler>. }\n" +
+                "}" +
+                "}}"
+        );
+        int counter1=0;
+        while (results2.hasNext()) {
+            results2.next();
+            counter1++;
+
+        }
+        System.out.println(counter1);
+
+        ResultSet results3 = makeQuery("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+                "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+                "PREFIX p: <http://www.wikidata.org/prop/>\n" +
+                "PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
+                "PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+                "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "PREFIX sch: <http://schema.org/>\n" +
+                "PREFIX yago: <http://yago-knowledge.org/resource/> \n" +
+                "SELECT DISTINCT\n" +
+                "        ?property\n" +
+                "WHERE{\n"+
+                "SERVICE <https://yago-knowledge.org/sparql/query>\n" +
+                "{\n" +
+                "SELECT DISTINCT ?property  {\n" +
+                "{ <http://yago-knowledge.org/resource/Adolf_Hitler> ?property ?value. }\n" +
+                "UNION\n" +
+                "{ ?value ?property <http://yago-knowledge.org/resource/Adolf_Hitler>. }\n" +
+                "}" +
+                "}}"
+        );
+        int counter2=0;
+        while (results3.hasNext()) {
+            results3.next();
+            counter2++;
+
+        }
+        System.out.println(counter2);
+        ResultSet results4 = makeQuery("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+                "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+                "PREFIX p: <http://www.wikidata.org/prop/>\n" +
+                "PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
+                "PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+                "prefix dbpedia: <http://dbpedia.org/resource/>\n" +
+                "prefix dbpedia-owl: <http://dbpedia.org/ontology/>\n" +
+                "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "PREFIX sch: <http://schema.org/>\n" +
+                "PREFIX yago: <http://yago-knowledge.org/resource/> \n" +
+                "SELECT\n" +
+                "        ?wikidataDescription\n" +
+                "        ?dbpediaDescription\n" +
+                "        ?yagoDescription\n" +
+                "WHERE{\n"+
+                "SERVICE <https://query.wikidata.org/sparql>\n" +
+                "{\n" +
+                "SELECT ?wikidataDescription WHERE {\n" +
+                " wd:Q352 sch:description ?wikidataDescription.\n" +
+                "  FILTER ( lang(?wikidataDescription) = \"en\" )" +
+                "}" +
+                "}"+
+                "SERVICE <https://dbpedia.org/sparql>\n" +
+                "{\n" +
+                "SELECT ?dbpediaDescription  WHERE { \n" +
+                "  dbpedia:Adolf_Hitler dbpedia-owl:abstract ?dbpediaDescription .\n" +
+                "\n" +
+                "  FILTER(langMatches(lang(?dbpediaDescription),\"en\"))\n" +
+                "}" +
+
+                "}" +
+
+                "SERVICE <https://yago-knowledge.org/sparql/query>\n" +
+                "{\n" +
+                "SELECT ?yagoDescription WHERE {\n" +
+                " <http://yago-knowledge.org/resource/Adolf_Hitler> rdfs:comment ?yagoDescription.\n" +
+                "  FILTER ( lang(?yagoDescription) = \"en\" )" +
+                "}" +
+                "}"+
+                "}"
+        );
+//        System.out.println(ResultSetFormatter.asText(results4));
+        Integer a=0;
+        Integer b=0;
+        Integer c=0;
+        while(results4.hasNext()){
+            QuerySolution next=results4.next();
+            a=next.get("wikidataDescription").toString().length();
+            b=next.get("dbpediaDescription").toString().length();
+            c=next.get("yagoDescription").toString().length();
+        }
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
    }}
 //    System.out.println(indexes);
 
